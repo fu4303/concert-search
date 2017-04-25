192c94f4327f61074ffcbb0ccbaaa2e7f589ec15
Kara Fulgum <karafulgum@gmail.com>
2017-04-25 14:05:35 -0500

    Nav HTML and styling

---
+++ app/app.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+  <div class="app">
+    <div class="hero">
+      <div class="container1">
+        <div class="nav">
+          <img src="./images/logo.png" alt="">
+          <div class="nav__right">
+            <a href="#" class="nav__item">Browse</a>
+            <a href="#" class="nav__item">Discover</a>
+            <a href="#" class="nav__item">Add Event</a>
+            <a href="#" class="nav__item nav__item--main">Join Us</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="app__main">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+export default Vue.extend({
+  data() {
+    return {};
+  },
+
+  methods: {
+
+  },
+});
+</script>
